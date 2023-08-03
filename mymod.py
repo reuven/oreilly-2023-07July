769d9ac6b7b3a99ad872e41619e675751348e479
Reuven Lerner <reuven@lerner.co.il>
2023-08-03 20:47:05 +0300

    gitautosync update 51

--- mymod.py
+++ mymod.py
@@ -1,3 +1,5 @@
+print(f'Hello from mymod!')
+
 x = 100
 
 y = [10, 20, 30]
@@ -6,4 +8,5 @@ d = {'a':100, 'b':200, 'c':300}   # I added this dict to mymod
 
 def hello(name):
     return f'Hello, {name}, from mymod!'
-    
+
+print(f'Goodbye from mymod!')
